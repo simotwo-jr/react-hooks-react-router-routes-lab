132eaa3bf74e5dfff05162b5d33bf42ba97ba593
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-02-22 10:17:07 +0300

    feat: Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,37 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+   
+      <div>
+        {/* Render the NavBar component */}
+        <NavBar />
+
+        {/* Use Switch to render the first matching Route */}
+        <Switch>
+          {/* Route for /movies */}
+          <Route path="/movies">
+            <Movies />
+          </Route>
+
+          {/* Route for /directors */}
+          <Route path="/directors">
+            <Directors />
+          </Route>
+
+          {/* Route for /actors */}
+          <Route path="/actors">
+            <Actors />
+          </Route>
+
+          {/* Default route for / or any other undefined route */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      {/* NavLink for Home */}
+      <NavLink to="/">Home</NavLink>
+
+      {/* NavLink for Movies */}
+      <NavLink to="/movies">Movies</NavLink>
+
+      {/* NavLink for Directors */}
+      <NavLink to="/directors">Directors</NavLink>
+
+      {/* NavLink for Actors */}
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
